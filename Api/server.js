031af6a4b8e92371a7c5704e1723a86f343817c4
Rana Usman <usman4243ch@gmail.com>
2023-12-10 02:29:35 -0800

    Establish Mongo connection

--- Api/server.js
+++ Api/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 
+dotenv.config();
 
 const app = express()
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB is connected...");
+  })
+  .catch((error) => {
+    console.error("Error in connecting to MongoDB:", error);
+  });
 
 
 const PORT = process.env.PORT || 4000;
