00954914c2d616b90a88e72e3f2fa71e25b1c3f8
Rana Usman <usman4243ch@gmail.com>
2023-12-11 14:19:53 -0800

    Complete Login Functionality

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Login.css'
 import { useAlert } from 'react-alert'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
@@ -9,6 +10,7 @@ const Login = () => {
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false)
     const alert = useAlert()
+    const navigate = useNavigate()
 
 
     const regClick = () => {
@@ -25,59 +27,108 @@ const Login = () => {
             [e.target.id]: e.target.value
         })
     }
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+
+    const loginChangeHandler = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    };
+
+    const registerSubmitHandler = async (e) => {
+        e.preventDefault();
         try {
-            setLoading(true)
-            const res = await fetch('http://localhost:4000/api/auth/signup',
-                {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(formData)
-                })
-            const data = await res.json()
-            console.log(data)
+            setLoading(true);
+            const res = await fetch('http://localhost:4000/api/auth/register', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            const data = await res.json();
             if (data.success === false) {
-                setLoading(false)
-                setError(error.message)
-                return
+                setLoading(false);
+                setError(data.message);
+                return;
             }
-            setLoading(false)
-            setError(null)
-            alert.success("User Created Successfully...")
+            setLoading(false);
+            setError(null);
+            alert.success("User Created Successfully...");
             setTrans(false);
         } catch (error) {
-            setLoading(false)
-            setError(error.message)
-            alert.error(error.message)
+            setLoading(false);
+            setError(error.message);
+            alert.error(error.message);
         }
-    }
+    };
+
+
+
+    const loginSubmitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const loginData = {
+                email: formData.loginUsername,
+                password: formData.loginPassword
+            };
+
+            const res = await fetch('http://localhost:4000/api/auth/login', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(loginData)
+            });
+
+            const data = await res.json();
+            if (data.success === false) {
+                setLoading(false);
+                setError(data.message);
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            alert.success("User Logged In Successfully...");
+            navigate("/");
+        } catch (error) {
+            setLoading(false);
+            setError(error.message);
+            alert.error(error.message);
+        }
+    };
+
 
     return (
         <div className='main-form'>
             <div className='login'>
                 <div className={`login ${trans ? 'trans' : ''}`} onClick={logClick}>
-                    <form>
+                    <form onSubmit={loginSubmitHandler}>
                         <h2 className={trans ? 'scaleDown' : ''}>Log In</h2>
                         <input
                             type='text'
                             placeholder='Username'
+                            onChange={loginChangeHandler}
+                            id='loginUsername'
                             required
                         />
                         <input
                             type='password'
                             placeholder='Password'
+                            onChange={loginChangeHandler}
+                            id='loginPassword'
                             required
                         />
+
                         <div className='submitBtn'>
-                            <button>Log In</button>
+                            <button type='submit'>{loading ? 'Loading...' : 'Log In'}</button>
                         </div>
                     </form>
+
                 </div>
                 <div className={`register ${trans ? 'transformed' : ''}`} onClick={regClick}>
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={registerSubmitHandler}>
                         <h2>Register</h2>
                         <input
                             type='text'
